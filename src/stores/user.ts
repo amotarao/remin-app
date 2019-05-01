6c5d208038c4212aa9509335cf8f6035b66a9696
Amon Sawamura <amon.sawamura@gmail.com>
2019-05-01 13:32:01 +0900

    feat(user): add state check

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -13,7 +13,18 @@ export class UserContainer extends Container<UserState> {
       signedIn: false,
       user: null,
     };
+    this.onAuthStateChanged();
   }
+
+  onAuthStateChanged = () => {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ ...this.state, signedIn: true, user });
+      } else {
+        this.setState({ ...this.state, signedIn: false, user: null });
+      }
+    });
+  };
 
   signIn = async (token: string) => await auth.signInWithCustomToken(token);
 }
