cd3cfc8a9989cfbf959f21e3582ec1134dd6c06e
Amon Sawamura <amon.sawamura@gmail.com>
2019-05-02 03:20:53 +0900

    feat(slack): add scopes, button

--- src/components/pages/Top/Top.tsx
+++ src/components/pages/Top/Top.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../../../logo.svg';
+import { slackScopes } from '../../../utils/slack';
 
 export interface TopProps {
   isLoading: boolean;
@@ -12,6 +13,19 @@ export const Top: React.FC<TopProps> = ({ isLoading, signedIn, signOut }) => {
   return (
     <div>
       <header>
+        <a
+          href={`https://slack.com/oauth/authorize?scope=${encodeURIComponent(
+            slackScopes.join(',')
+          )}&client_id=${process.env.REACT_APP_SLACK_CLIENT_ID}`}
+        >
+          <img
+            alt="Add to Slack"
+            height="40"
+            width="139"
+            src="https://platform.slack-edge.com/img/add_to_slack.png"
+            srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+          />
+        </a>
         <img src={logo} alt="logo" />
         <p>{isLoading ? '読み込み中' : '読み込み完了'}</p>
         <p>{signedIn ? 'ログイン済' : 'ログインしていません'}</p>
