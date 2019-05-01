b3092648cf4444f5c6b9828dae71bd006128ea7d
Amon Sawamura <amon.sawamura@gmail.com>
2019-05-02 03:20:09 +0900

    chore(css): remove css modules

--- src/components/pages/Top/Top.tsx
+++ src/components/pages/Top/Top.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from '../../../logo.svg';
-import '../../../App.css';
 
 export interface TopProps {
   isLoading: boolean;
@@ -11,18 +10,13 @@ export interface TopProps {
 
 export const Top: React.FC<TopProps> = ({ isLoading, signedIn, signOut }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
+        <img src={logo} alt="logo" />
         <p>{isLoading ? '読み込み中' : '読み込み完了'}</p>
         <p>{signedIn ? 'ログイン済' : 'ログインしていません'}</p>
         <button onClick={signOut}>ログアウト</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
