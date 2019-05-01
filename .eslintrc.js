4286e4704559e94b0c2d61c620278b5ce82c8639
Amon Sawamura <amon.sawamura@gmail.com>
2019-05-01 13:53:57 +0900

    chore(eslint): add react property

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,40 @@
 module.exports = {
   "extends": [
     "eslint:recommended",
+    "plugin:react/recommended",
     "plugin:prettier/recommended",
   ],
   "plugins": [
     "@typescript-eslint",
+    "react",
   ],
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
     "sourceType": "module",
     "project": "./tsconfig.json",
   },
+  "env": {
+    "browser": true,
+    "node": true,
+  },
   "rules": {
-    'prettier/prettier': [
-      'error',
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "react/prop-types": "off",
+    "prettier/prettier": [
+      "error",
       {
-        'singleQuote': true,
-        'semi': true,
-        'trailingComma': 'es5',
+        "singleQuote": true,
+        "semi": true,
+        "trailingComma": "es5",
       },
     ],
-    'indent': ['error', 2],
-    'linebreak-style': ['error', 'unix'],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "single"],
+    "semi": ["error", "always"],
   },
 }
