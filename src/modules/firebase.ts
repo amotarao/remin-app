dd65f7f0a916643f2f106328a95e20b4b8277c68
Amon Sawamura <amon.sawamura@gmail.com>
2019-05-12 10:02:20 +0900

    fix(firebase): env to .env

--- src/modules/firebase.ts
+++ src/modules/firebase.ts
@@ -3,12 +3,12 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyAhY1xwR0C2sgLULUjedH0gKJcGZ6Ml60U',
-  authDomain: 'remin-app.firebaseapp.com',
-  databaseURL: 'https://remin-app.firebaseio.com',
-  projectId: 'remin-app',
-  storageBucket: 'remin-app.appspot.com',
-  messagingSenderId: '221657618594',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 });
 
 export const auth = firebase.auth();
